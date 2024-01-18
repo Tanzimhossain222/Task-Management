2787584a184e7744c3ac4fe40e617a64d7bad08c
Tanzim Hossain <tanzimhossain2@gmail.com>
2024-01-18 12:12:31 +0600

    TaskBoard UI setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
+import TaskBoard from "./components/Task/TaskBoard";
 import Footer from "./components/footer";
 import HeroSection from "./components/heroSection";
 import Header from "./components/navbar";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
-      <HeroSection />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
       <Footer />
-    </div>
+    </>
   );
 };
 
